5ee8ecdd034fb33c3d06d212bbdf7b220dda3d9b
Naveen Nathan <naveen.nathan@tredence.com>
2018-03-05 00:15:04 +0530

    Adding .Rproj and deep learning exercise using iris data set.

---
+++ iris_deeplearning.R
@@ -0,0 +1,19 @@
+library(h2o)
+
+h2o.init()
+url <- "http://h2o-public-test-data.s3.amazonaws.com/smalldata/iris/iris_wheader.csv"
+iris <- h2o.importFile(url)
+
+parts <- h2o.splitFrame(iris, 0.8)
+train <- parts[[1]]
+test <- parts[[2]]
+
+summary(train)
+
+dl_model <- h2o.deeplearning(1:4, 5, train)
+dl_model
+summary(dl_model)
+
+preds <- h2o.predict(dl_model, test)
+
+h2o.performance(dl_model, test)
